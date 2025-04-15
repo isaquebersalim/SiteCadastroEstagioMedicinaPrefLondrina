485736e1db296a38c423da417bb0c4e37a336b9c
Isaque Bersalim <isaquebersalim@gmail.com>
2025-04-15 19:01:48 -0300

    Criado o JavaScript da página

---
+++ script.js
@@ -0,0 +1,6 @@
+function showForm(id) {
+    document.querySelectorAll('section').forEach(section => {
+      section.classList.remove('active');
+    });
+    document.getElementById(id).classList.add('active');
+  }
